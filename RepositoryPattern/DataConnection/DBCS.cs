2d231bede111e8a97c9f138cedf2a4781693079a
jil <jil.p@simformsolutions.com>
2023-09-12 10:00:10 +0530

    add: repository pattern library

---
+++ RepositoryPattern/DataConnection/DBCS.cs
@@ -0,0 +1,9 @@
+﻿namespace RepositoryPattern.DataConnection;
+
+public static class DBCS
+{
+    public static string ConnectionString()
+    {
+        return "Data Source=SF-CPU-312\\SQLEXPRESS;Initial Catalog=Employee;User ID=abc;Password=123123;Connect Timeout=30;Encrypt=False";
+    }
+}
